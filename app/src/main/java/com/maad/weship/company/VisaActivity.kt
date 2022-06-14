71222bec88b69ca33b9868246193c1d01eb6dbd3
AhmedMaad <atef.ahmed1341@gmail.com>
2022-06-14 15:02:13 +0200

    Add visa design

---
+++ app/src/main/java/com/maad/weship/company/VisaActivity.kt
@@ -0,0 +1,19 @@
+package com.maad.weship.company
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.maad.weship.R
+
+class VisaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_visa)
+
+        val visaPay: Button = findViewById(R.id.pay_btn)
+        visaPay.setOnClickListener {
+            //TODO: Add request to history
+        }
+
+    }
+}
